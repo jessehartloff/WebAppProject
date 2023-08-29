252100cad6d2788324111ef025be551015f09991
jessehartloff <hartloff@buffalo.edu>
2023-08-28 23:26:35 -0400

    Initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+import socketserver
+import sys
+from util.request import Request
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        received_data = self.request.recv(2048)
+        print(self.client_address)
+        print("--- received data ---")
+        print(received_data)
+        print("--- end of data ---\n\n")
+        request = Request(received_data)
+
+        # TODO: Parse the HTTP request and use self.request.sendall(response) to send your response
+
+
+def main():
+    host = "0.0.0.0"
+    port = 8080
+
+    socketserver.TCPServer.allow_reuse_address = True
+
+    server = socketserver.TCPServer((host, port), MyTCPHandler)
+
+    print("Listening on port " + str(port))
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
---
+++ util/request.py
@@ -0,0 +1,10 @@
+class Request:
+
+    def __init__(self, request: bytes):
+        # TODO: parse the bytes of the request and populate the following instance variables
+
+        self.body = b""
+        self.method = ""
+        self.path = ""
+        self.http_version = ""
+        self.headers = {}
